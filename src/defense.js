19351a40af70ca83693b3428a7d214fa25b839a9
Kris Arnott <nightred@gmail.com>
2017-03-17 07:05:33 -0500

    this.energyTargets.push(

---
+++ src/defense.js
@@ -0,0 +1,34 @@
+/*
+ * Defense system
+ *
+ * defense provides monitoring of rooms for invaders (npc or player)
+ * and respondes to the threats
+ *
+ */
+
+var Defense = function() {
+    this.status = 'clear';
+};
+
+Defense.prototype.scanRoom = function(room) {
+    if (!room) { return -1; }
+
+    let targets = room.getHostiles();
+    if (targets.length <= 0) { return true; }
+    if (Game.Queues.work.isQueued({ task: 'defense', room: room.name, })) {
+        return true;
+    }
+
+    let record = {
+        workRooms: [ room.name, ],
+        task: 'defense',
+        managed: true,
+        priority: 10,
+        creepLimit: 0,
+    };
+    Game.Queues.work.addRecord(record);
+
+    return true;
+};
+
+module.exports = Defense;
