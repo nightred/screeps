a37b4f27f07936b5c48dc95aacb251fd93f80064
Kris Arnott <nightred@gmail.com>
2017-04-03 10:04:42 -0500

    role linker

---
+++ src/role/linker.js
@@ -0,0 +1,144 @@
+/*
+ * Linker Role
+ *
+ * miner role that handles moving energy in a room
+ *
+ */
+
+var roleLinker = {
+
+    /**
+    * The role name
+    **/
+    role: C.LINKER,
+
+    /** @param {Creep} creep **/
+    doRole: function(creep) {
+        if (!creep) { return -1; }
+
+        if (creep.manageState()) {
+            if (creep.memory.working) {
+                creep.say('🚚');
+            } else {
+                creep.say('🔋');
+                creep.memory.task = false;
+            }
+        } else if (creep.carry.energy > (creep.carryCapacity * 0.2) && !creep.memory.working)  {
+            creep.toggleState();
+            creep.say('🚚');
+        }
+
+        if ((creep.memory.idleStart + C.CREEP_IDLE_TIME) > Game.time) {
+            if (creep.memory.task) {
+                creep.memory.task = false;
+            }
+            return true;
+        }
+
+        if (!creep.memory.task) {
+            if (!this.getTask(creep))  {
+                creep.memory.idleStart = Game.time;
+                creep.say('💤');
+                return true;
+            }
+        }
+
+        let energyInTargets = [];
+        let energyOutTargets = [];
+
+        switch (creep.memory.task) {
+            case 'store':
+                energyInTargets = ['linkStorage',];
+                energyOutTargets = ['storage',];
+                break;
+            case 'fill':
+                energyOutTargets = ['linkStorage',];
+                energyInTargets = ['storage',];
+                break;
+        }
+
+        // working has energy, else need energy
+        if (creep.memory.working) {
+            if (!creep.doEmptyEnergy(energyOutTargets)) {
+                if (C.DEBUG >= 2) { console.log('DEBUG - do empty energy failed for role: ' + creep.memory.role + ', name: ' + creep.name); }
+            }
+        } else {
+            if (!creep.doFillEnergy(energyInTargets)) {
+                if (C.DEBUG >= 2) { console.log('DEBUG - do fill energy failed for role: ' + creep.memory.role + ', name: ' + creep.name); }
+            }
+        }
+
+        return true;
+    },
+
+    /**
+    * Create the body of the creep for the role
+    * @param {number} energy The amount of energy avalible
+    * @param {Object} args Extra arguments
+    **/
+    getBody: function(energy, args) {
+        if (isNaN(energy)) { return -1; }
+        args = args || {};
+
+        let move = Math.floor((energy / 2) / 50);
+        move = move < 1 ? 1 : move;
+        move = move > 4 ? 4 : move;
+        energy -= move * 50;
+        let carry = Math.floor(energy / 50);
+        carry = carry < 1 ? 1 : carry;
+        carry = carry > 8 ? 8 : carry;
+
+        let body = [];
+        for (let i = 0; i < move; i++) {
+            body.push(MOVE);
+        }
+
+        for (let i = 0; i < carry; i++) {
+            body.push(CARRY);
+        }
+
+        return body;
+    },
+
+    /**
+    * Spawn the creep
+    * @param {Spawn} spawn The spawn to be used
+    * @param {array} body The creep body
+    * @param {Object} args Extra arguments
+    **/
+    doSpawn: function(spawn, body, args) {
+        if (!spawn) { return -1; }
+        if (!Array.isArray(body) || body.length < 1) { return -1; }
+        args = args || {};
+        args.role = this.role;
+
+        return spawn.createCreep(body, undefined, args);
+    },
+
+    getTask: function(creep) {
+        if (!creep) { return -1; }
+        let room = creep.room;
+
+        if (!room.storage) { return false }
+
+        let linksStorage = _.filter(room.getLinks(), structure =>
+            structure.memory.type == 'storage');
+        if (linksStorage.length <= 0) { return false; }
+        let linkStorage = linksStorage[0];
+
+        if (linkStorage.energy > (linkStorage.energyCapacity * C.ENERGY_LINK_STORAGE_MAX)) {
+            creep.memory.task = 'store';
+            return true;
+        }
+
+        if (linkStorage.energy < (linkStorage.energyCapacity * C.ENERGY_LINK_STORAGE_MIN)) {
+            creep.memory.task = 'fill';
+            return true;
+        }
+
+        return false;
+    },
+
+};
+
+module.exports = roleLinker;
---
+++ src/task/director/linker.js
@@ -0,0 +1,84 @@
+/*
+ * task Director Linker
+ *
+ * director linker task handles spawning of linker creeps
+ *
+ */
+
+var taskDirectorLinker = {
+
+    /**
+    * @param {Creep} creep The creep object
+    * @param {Task} task The work task passed from the work Queue
+    **/
+    doTask: function(creep, task) {
+        if (!creep) { return -1; }
+        if (!task) { return -1; }
+        // run creep task
+    },
+
+    /**
+    * @param {Task} task The work task passed from the work Queue
+    **/
+    doTaskManaged: function(task) {
+        if (!task) { return -1; }
+
+        task.manageTick = task.manageTick || 0;
+        if ((task.manageTick + C.MANAGE_WAIT_TICKS) > Game.time) {
+            return true;
+        }
+        task.manageTick = Game.time;
+
+        if (task.workRooms.length <= 0) {
+            if (C.DEBUG >= 2) { console.log('DEBUG - missing work rooms on task: ' + task.task + ', id: ' + task.id); }
+            return false;
+        }
+
+        let room = Game.rooms[task.workRooms[0]];
+        if (!room) {
+            if (C.DEBUG >= 3) { console.log('VERBOSE - no eyes on room: ' + task.workRooms[0] + ', task: ' + task.task + ', id: ' + task.id); }
+            return true;
+        }
+
+        let linksStorage = _.filter(room.getLinks(), structure =>
+            structure.memory.type == 'storage');
+        if (!room.storage || linksStorage.length <= 0) {
+            task.creepLimit = task.creepLimit > 0 ? 0 : task.creepLimit;
+        } else {
+            task.creepLimit = task.creepLimit < 1 ? 1 : task.creepLimit;
+
+            // spawn new creeps if needed
+            count = _.filter(Game.creeps, creep =>
+                creep.memory.directorId == task.id &&
+                creep.memory.despawn != true
+                ).length;
+            if (count < task.creepLimit) {
+                if (!Game.Queue.spawn.isQueued({ room: task.spawnRoom, role: C.LINKER, })) {
+                    let record = {
+                        rooms: [ task.spawnRoom, ],
+                        role: C.LINKER,
+                        priority: 49,
+                        creepArgs: {
+                            workRooms: task.workRooms,
+                            directorId: task.id,
+                        },
+                    };
+                    Game.Queue.spawn.addRecord(record);
+                }
+            }
+        }
+
+        return true;
+    },
+
+    /**
+    * @param {Room} room The room object
+    **/
+    doTaskFind: function(room) {
+        if (!room) { return -1; }
+        // task creation for the room
+    },
+
+};
+
+module.exports = taskDirectorLinker;
