c0b1bb8fcdd7e9bd03be46380c72a790fdd06a6a
Kris Arnott <nightred@gmail.com>
2017-09-15 07:34:46 -0500

    add mineral miner

--- src/processes/tasks/mineral.js
+++ src/processes/tasks/mineral.js
@@ -11,56 +11,72 @@ var taskMineral = function() {
 
 taskMineral.prototype.run = function() {
     let creep = Game.creeps[this.memory.creepName];
-
     if (!creep) {
         Game.kernel.killProcess(this.pid);
         return;
     }
 
-    if (creep.getOffExit()) {
-        return;
-    }
+    if (creep.getOffExit()) return;
 
     if (creep.isSleep()) {
         creep.moveToIdlePosition();
         return;
     }
 
-    if (creep.room.name != task.workRoom) {
-        creep.moveToRoom(task.workRoom);
-        return true;
+    if (creep.room.name != creep.memory.workRooms) {
+        creep.moveToRoom(creep.memory.workRooms);
+        return;
     }
 
-    let extractor = Game.getObjectById(creep.extractorId);
+    if ((creep.carryCapacity * 0.8) > _.sum(creep.carry) || creep.carryCapacity == 0) {
+        this.doWork(creep);
+    } else {
+        this.doEmpty(creep);
+    }
+};
+
+/**
+* @param {Creep} creep The creep object
+**/
+taskMineral.prototype.doEmpty = function(creep) {
+    let energyTargets = [
+        'containerIn',
+        'storage',
+    ];
+
+    let mineral = Game.getObjectById(creep.memory.mineralId);
+
+    if (!creep.memory.goingTo && mineral) {
+        creep.memory.goingTo = mineral.getLocalContainer();
+    }
+
+    creep.doEmpty(energyTargets, RESOURCE_ENERGY);
+};
+
+/**
+* @param {Creep} creep The creep object
+**/
+taskMineral.prototype.doWork = function(creep) {
+    let extractor = Game.getObjectById(creep.memory.extractorId);
     if (!extractor) {
-        if (C.DEBUG >= 3) { console.log('VERBOSE - extractor missing in room: ' + creep.room.name + ', creep: ' + creep.name); }
         creep.doDespawn();
-        return false;
+        return;
     }
-    if (extractor.cooldown > 0) {
-        return true;
-    }
+    if (extractor.cooldown && extractor.cooldown > 0) return;
 
-    let mineral = Game.getObjectById(creep.mineralId);
-    if (!mineral) {
-        if (C.DEBUG >= 3) { console.log('VERBOSE - mineral missing in room: ' + creep.room.name + ', creep: ' + creep.name); }
-        creep.doDespawn();
-        return false;
-    }
+    let mineral = Game.getObjectById(creep.memory.mineralId);
 
     if (!creep.pos.inRangeTo(mineral, 1)) {
-        let args = {
+        creep.goto(mineral, {
             range: 1,
             reusePath: 30,
             maxRooms: 1,
             ignoreCreeps: true,
-        };
-        creep.goto(mineral, args);
-        return true;
+        });
+        return;
     }
 
     creep.harvest(mineral)
-    return true;
 };
 
 registerProcess('tasks/mineral', taskMineral);
