a6a3b1310a59758d9e633f21c0be1de468901ab5
Kris Arnott <nightred@gmail.com>
2017-10-26 14:00:45 -0500

    spawn details update

--- src/processes/tasks/militia.js
+++ src/processes/tasks/militia.js
@@ -37,6 +37,10 @@ taskMilitia.prototype.run = function() {
     }
 };
 
+taskMilitia.prototype.getAssignedRoom = function() {
+
+};
+
 /**
 * @param {Creep} creep The creep object
 **/
@@ -89,30 +93,13 @@ taskMilitia.prototype.doCreepDefense = function(creep) {
 };
 
 taskMilitia.prototype.doSpawnDetails = function() {
-    if (this.memory._sleepSpawnDetails && this.memory._sleepSpawnDetails > Game.time) return;
-    this.memory._sleepSpawnDetails = Game.time + (C.TASK_SPAWN_DETAILS_SLEEP + Math.floor(Math.random() * 20));
-
-    let spawnRoom = Game.rooms[this.memory.spawnRoom];
-    if (!spawnRoom || !spawnRoom.controller || !spawnRoom.controller.my) return;
-
-    let limit = 0;
-    let clevel = spawnRoom.controller.level;
-    if (clevel == 2 || clevel == 3) {
-        limit = 1;
-    } else if (clevel == 4 || clevel == 5 || clevel == 6) {
-        limit = 1;
-    } else if (clevel == 7) {
-       limit = 1;
-    } else if (clevel == 8) {
-        limit = 2;
-    }
-
+    if (this.memory.spawnDetails) return;
     this.setSpawnDetails({
         role: C.ROLE_COMBAT_MILITIA,
         priority: 38,
         spawnRoom: this.memory.spawnRoom,
         creepArgs: {},
-        limit: limit,
+        limit: 1,
     });
 };
 
